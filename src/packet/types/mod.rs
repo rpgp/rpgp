2f91914751b81cb971c4789f879351dbb92ff3e2
dignifiedquire <dignifiedquire@gmail.com>
2018-07-01 20:08:58 +0200

    implement wider ecc curve support
    Former-commit-id: 748288e59c9a02cc7d98f1404eac7f2bd46cd882

--- src/packet/types/mod.rs
+++ src/packet/types/mod.rs
@@ -3,16 +3,167 @@ use std::collections::HashMap;
 
 pub mod pubkey;
 
-#[derive(Debug, PartialEq, Eq, Clone)]
-/// Available ECC Curves
-/// Ref: https://tools.ietf.org/html/rfc6637#section-11
+// const OID_ECC_P256: [u8; 8] = [0x2A, 0x86, 0x48, 0xCE, 0x3D, 0x03, 0x01, 0x07];
+// const OID_ECC_P384: [u8; 5] = [0x2B, 0x81, 0x04, 0x00, 0x22];
+// const OID_ECC_P521: [u8; 5] = [0x2B, 0x81, 0x04, 0x00, 0x23];
+
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub enum ECCCurve {
-    /// NIST Curve P-256, oid 0x2A8648CE3D030107
+    Curve25519,
+    Ed25519,
     P256,
-    /// NIST Curve P-384, oid 0x2B81040022
     P384,
-    /// NIST Curve P-521, oid 0x2B81040023
     P521,
+    BrainpoolP256r1,
+    BrainpoolP384r1,
+    BrainpoolP512r1,
+    Secp256k1,
+}
+
+impl ECCCurve {
+    /// Standard name
+    pub fn name(&self) -> &str {
+        match *self {
+            ECCCurve::Curve25519 => "Curve25519",
+            ECCCurve::Ed25519 => "Ed25519",
+            ECCCurve::P256 => "NIST P-256",
+            ECCCurve::P384 => "NIST P-384",
+            ECCCurve::P521 => "NIST P-521",
+            ECCCurve::BrainpoolP256r1 => "brainpoolP256r1",
+            ECCCurve::BrainpoolP384r1 => "brainpoolP384r1",
+            ECCCurve::BrainpoolP512r1 => "brainpool5126r1",
+            ECCCurve::Secp256k1 => "secp256k1",
+        }
+    }
+
+    /// IETF formatted OID
+    pub fn oid_str(&self) -> &str {
+        match *self {
+            ECCCurve::Curve25519 => "1.3.6.1.4.1.3029.1.5.1",
+            ECCCurve::Ed25519 => "1.3.6.1.4.1.11591.15.1",
+            ECCCurve::P256 => "1.2.840.10045.3.1.7",
+            ECCCurve::P384 => "1.3.132.0.34",
+            ECCCurve::P521 => "1.3.132.0.35",
+            ECCCurve::BrainpoolP256r1 => "1.3.36.3.3.2.8.1.1.7",
+            ECCCurve::BrainpoolP384r1 => "1.3.36.3.3.2.8.1.1.11",
+            ECCCurve::BrainpoolP512r1 => "1.3.36.3.3.2.8.1.1.13",
+            ECCCurve::Secp256k1 => "1.3.132.0.10",
+        }
+    }
+
+    /// Nominal bit length of the curve
+    pub fn nbits(&self) -> u16 {
+        match *self {
+            ECCCurve::Curve25519 => 255,
+            ECCCurve::Ed25519 => 255,
+            ECCCurve::P256 => 256,
+            ECCCurve::P384 => 384,
+            ECCCurve::P521 => 521,
+            ECCCurve::BrainpoolP256r1 => 256,
+            ECCCurve::BrainpoolP384r1 => 384,
+            ECCCurve::BrainpoolP512r1 => 512,
+            ECCCurve::Secp256k1 => 256,
+        }
+    }
+
+    /// Alternative name of the curve
+    pub fn alias(&self) -> Option<&str> {
+        match *self {
+            ECCCurve::Curve25519 => Some("cv25519"),
+            ECCCurve::Ed25519 => Some("ed25519"),
+            ECCCurve::P256 => Some("nistp256"),
+            ECCCurve::P384 => Some("nistp384"),
+            ECCCurve::P521 => Some("nistp521"),
+            ECCCurve::BrainpoolP256r1 => None,
+            ECCCurve::BrainpoolP384r1 => None,
+            ECCCurve::BrainpoolP512r1 => None,
+            ECCCurve::Secp256k1 => None,
+        }
+    }
+
+    /// Required algo, or None for ECDSA/ECDH
+    pub fn pubkey_algo(&self) -> Option<PublicKeyAlgorithm> {
+        match *self {
+            ECCCurve::Curve25519 => Some(PublicKeyAlgorithm::ECDH),
+            ECCCurve::Ed25519 => Some(PublicKeyAlgorithm::EdDSA),
+            ECCCurve::P256 => None,
+            ECCCurve::P384 => None,
+            ECCCurve::P521 => None,
+            ECCCurve::BrainpoolP256r1 => None,
+            ECCCurve::BrainpoolP384r1 => None,
+            ECCCurve::BrainpoolP512r1 => None,
+            ECCCurve::Secp256k1 => None,
+        }
+    }
+
+    pub fn oid(&self) -> Vec<u8> {
+        // the OID String is turned into bytes
+        // with the first two numbers combined
+        let mut id: Vec<u32> = self
+            .oid_str()
+            .split('.')
+            .map(|v| v.parse::<u32>().unwrap())
+            .collect();
+
+        // combine the first two
+        let first = id.remove(0) * 40 + id.remove(0);
+        id.insert(0, first);
+
+        id.iter()
+            .flat_map(|ident| asn1_der_object_id_val_enc(*ident))
+            .collect()
+    }
+}
+/// Get the right curve given an oid.
+pub fn ecc_curve_from_oid(oid: &[u8]) -> Option<ECCCurve> {
+    if ECCCurve::Curve25519.oid().as_slice() == oid {
+        return Some(ECCCurve::Curve25519);
+    }
+    if ECCCurve::Ed25519.oid().as_slice() == oid {
+        return Some(ECCCurve::Ed25519);
+    }
+    if ECCCurve::P256.oid().as_slice() == oid {
+        return Some(ECCCurve::P256);
+    }
+    if ECCCurve::P384.oid().as_slice() == oid {
+        return Some(ECCCurve::P384);
+    }
+    if ECCCurve::P521.oid().as_slice() == oid {
+        return Some(ECCCurve::P521);
+    }
+    if ECCCurve::BrainpoolP256r1.oid().as_slice() == oid {
+        return Some(ECCCurve::BrainpoolP256r1);
+    }
+    if ECCCurve::BrainpoolP384r1.oid().as_slice() == oid {
+        return Some(ECCCurve::BrainpoolP384r1);
+    }
+    if ECCCurve::BrainpoolP512r1.oid().as_slice() == oid {
+        return Some(ECCCurve::BrainpoolP512r1);
+    }
+    if ECCCurve::Secp256k1.oid().as_slice() == oid {
+        return Some(ECCCurve::Secp256k1);
+    }
+    None
+}
+
+fn asn1_der_object_id_val_enc(val: u32) -> Vec<u8> {
+    let mut val = val;
+    let mut acc = Vec::new();
+    acc.push((val & 0x7f) as u8);
+    val >>= 7;
+
+    while val > 0 {
+        acc.insert(0, (0x80 | (val & 0x7f)) as u8);
+        val >>= 7;
+    }
+
+    acc
+}
+
+impl ToString for ECCCurve {
+    fn to_string(&self) -> String {
+        self.name().to_string()
+    }
 }
 
 #[derive(Debug, PartialEq, Eq, Clone)]
@@ -391,14 +542,18 @@ pub enum PublicKeyAlgorithm {
     ElgamalSign = 16,
     /// DSA (Digital Signature Algorithm) [FIPS186] [HAC]
     DSA = 17,
-    /// RESERVED: Elliptic Curve
+    /// Elliptic Curve: RFC-6637
     ECDH = 18,
-    /// RESERVED: ECDSA
+    /// ECDSA: RFC-6637
     ECDSA = 19,
     /// DEPRECATED: Elgamal (Encrypt and Sign)
     Elgamal = 20,
     /// Reserved for Diffie-Hellman (X9.42, as defined for IETF-S/MIME)
     DiffieHellman = 21,
+    /// EdDSA (not yet assigned)
+    EdDSA = 22,
+    /// Private (from OpenGPG)
+    Private = 110,
 }
 }
 
@@ -606,5 +761,33 @@ impl PrimaryKey {
         y: Vec<u8>,
     ) -> Self {
         Self::from_public_key(PublicKey::new_elgamal(ver, alg, p, g, y))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ecc_curve_to_oid() {
+        assert_eq!(
+            ECCCurve::P256.oid(),
+            vec![0x2A, 0x86, 0x48, 0xCE, 0x3D, 0x03, 0x01, 0x07]
+        );
+        assert_eq!(ECCCurve::P384.oid(), vec![0x2B, 0x81, 0x04, 0x00, 0x22]);
+    }
+
+    #[test]
+    fn test_ecc_curve_from_oid() {
+        let one = vec![0x2A, 0x86, 0x48, 0xCE, 0x3D, 0x03, 0x01, 0x07];
+        assert_eq!(ecc_curve_from_oid(one.as_slice()).unwrap(), ECCCurve::P256);
+
+        assert_eq!(ecc_curve_from_oid(vec![1, 2, 3].as_slice()), None);
+    }
+
+    #[test]
+    fn test_asn1_der_object_id_val_enc() {
+        assert_eq!(asn1_der_object_id_val_enc(840), vec![0x86, 0x48]);
+        assert_eq!(asn1_der_object_id_val_enc(113549), vec![0x86, 0xf7, 0x0d]);
     }
 }
