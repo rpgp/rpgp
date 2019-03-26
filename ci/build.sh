7e426e9250b50fa02ce5d0bba57d21a21637ee3b
dignifiedquire <dignifiedquire@users.noreply.github.com>
2019-03-26 21:32:47 +0100

    fix
    Former-commit-id: 2944771d50cf8c3a462dac581b2e276bb3e8b1a0

--- ci/build.sh
+++ ci/build.sh
@@ -24,31 +24,31 @@ if [[ $CARGO_CMD == "cargo" ]]; then
 fi
 
 if [[ $TARGET = *"ios"* ]]; then
-    cargo install lippo --force
+    cargo install cargo-lippo --force
 
     RUSTFLAGS="-C codegen-units=1 -C lto=thin" cargo lippo --release --features nightly -p pgp_ffi
     cp -r target/universal/release/ .
 
 elif [[ $TARGET = *"windows"* ]]; then
     cargo build --release --features nightly -p pgp_ffi --target $TARGET
-    mkdir -p release/$@/lib/pkgconfig release/$@/include
-    cp target/release/libpgp_ffi.dll release/$@/lib/librpgp.dll
-    cp librpgp.h release/$@/include/
-    cp target/release/pkgconfig/rpgp.pc release/$@/lib/pkgconfig
+    mkdir -p release/lib/pkgconfig release/include
+    cp target/release/libpgp_ffi.dll release/lib/librpgp.dll
+    cp librpgp.h release/include/
+    cp target/release/pkgconfig/rpgp.pc release/lib/pkgconfig
 
 elif [[ $TARGET = *"darwin"* ]]; then
     cargo build --release --features nightly -p pgp_ffi --target $TARGET
-    mkdir -p release/$@/lib/pkgconfig release/$@/include
-    cp target/release/libpgp_ffi.dylib release/$@/lib/librpgp.dylib
-    cp librpgp.h release/$@/include/
-    cp target/release/pkgconfig/rpgp.pc release/$@/lib/pkgconfig
+    mkdir -p release/lib/pkgconfig release/include
+    cp target/release/libpgp_ffi.dylib release/lib/librpgp.dylib
+    cp librpgp.h release/include/
+    cp target/release/pkgconfig/rpgp.pc release/lib/pkgconfig
 else
     # nix systems
     RUSTFLAGS="-C codegen-units=1" cross build --release --features nightly -p pgp_ffi --target $TARGET
-    mkdir -p release/$@/lib/pkgconfig release/$@/include
-    cp target/release/libpgp_ffi.so release/$@/lib/librpgp.so
-    cp librpgp.h release/$@/include/
-    cp target/release/pkgconfig/rpgp.pc release/$@/lib/pkgconfig
+    mkdir -p release/lib/pkgconfig release/include
+    cp target/release/libpgp_ffi.so release/lib/librpgp.so
+    cp librpgp.h release/include/
+    cp target/release/pkgconfig/rpgp.pc release/lib/pkgconfig
 fi
 
 
