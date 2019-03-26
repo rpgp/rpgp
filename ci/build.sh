4bb43726b1fbe10ae0f323e02f759be9de8ace0a
dignifiedquire <dignifiedquire@users.noreply.github.com>
2019-03-26 22:30:53 +0100

    simplify build script
    Former-commit-id: 73ba0a08e4dc1ddea33e6377f43c32b577bbadaa

--- ci/build.sh
+++ ci/build.sh
@@ -3,33 +3,17 @@
 set -ex
 
 export RUST_BACKTRACE=1
-export RUST_TEST_NOCAPTURE=1
-export OPT="-p pgp_ffi --target=$TARGET"
-
-# Select cargo command: use cross by default
-export CARGO_CMD=cross
-
-if [[ $TARGET = *"windows"* ]] || [[ $TARGET = *"darwin"* ]] || [ $TARGET = *"ios"* ]; then
-    export CARGO_CMD=cargo
-fi
-
-# Install cross if necessary:
-if [[ $CARGO_CMD == "cross" ]]; then
-    cargo install --git https://github.com/dignifiedquire/cross --rev fix-tty --force
-fi
-
-# Make sure TARGET is installed when using cargo:
-if [[ $CARGO_CMD == "cargo" ]]; then
-    rustup target add $TARGET || true
-fi
 
 if [[ $TARGET = *"ios"* ]]; then
+    rustup target add $TARGET || true
     cargo install cargo-lipo --force
 
     RUSTFLAGS="-C codegen-units=1 -C lto=thin" cargo lipo --release --features nightly -p pgp_ffi
     cp -r target/universal/release/ .
 
 elif [[ $TARGET = *"windows"* ]]; then
+    rustup target add $TARGET || true
+
     cargo build --release --features nightly -p pgp_ffi --target $TARGET
     mkdir -p release/lib/pkgconfig release/include
     cp "target/${TARGET}release/libpgp_ffi.dll" release/lib/librpgp.dll
@@ -37,6 +21,8 @@ elif [[ $TARGET = *"windows"* ]]; then
     cp "target/${TARGET}/release/pkgconfig/rpgp.pc" release/lib/pkgconfig
 
 elif [[ $TARGET = *"darwin"* ]]; then
+    rustup target add $TARGET || true
+
     cargo build --release --features nightly -p pgp_ffi --target $TARGET
     mkdir -p release/lib/pkgconfig release/include
     cp "target/${TARGET}release/libpgp_ffi.dylib" release/lib/librpgp.dylib
@@ -44,6 +30,9 @@ elif [[ $TARGET = *"darwin"* ]]; then
     cp "target/${TARGET}/release/pkgconfig/rpgp.pc" release/lib/pkgconfig
 else
     # nix systems
+
+    cargo install --git https://github.com/dignifiedquire/cross --rev fix-tty --force
+
     RUSTFLAGS="-C codegen-units=1" cross build --release --features nightly -p pgp_ffi --target $TARGET
     mkdir -p release/lib/pkgconfig release/include
     cp "target/${TARGET}release/libpgp_ffi.so" release/lib/librpgp.so
