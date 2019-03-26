790ed7c59a1e3773a5827a3ae03ad8550c21786d
dignifiedquire <dignifiedquire@users.noreply.github.com>
2019-03-26 22:09:13 +0100

    bash is hard
    Former-commit-id: b2c34340f3faaee854ec18624754b0f0be227c0c

--- ci/build.sh
+++ ci/build.sh
@@ -9,7 +9,7 @@ export OPT="-p pgp_ffi --target=$TARGET"
 # Select cargo command: use cross by default
 export CARGO_CMD=cross
 
-if [[ $TARGET = *"windows"* ]] || [[ $TARGET = *"darwin"* ]] || [ $TARGET = *"ios"*]; then
+if [[ $TARGET = *"windows"* ]] || [[ $TARGET = *"darwin"* ]] || [ $TARGET = *"ios"* ]; then
     export CARGO_CMD=cargo
 fi
 
@@ -32,28 +32,28 @@ if [[ $TARGET = *"ios"* ]]; then
 elif [[ $TARGET = *"windows"* ]]; then
     cargo build --release --features nightly -p pgp_ffi --target $TARGET
     mkdir -p release/lib/pkgconfig release/include
-    cp "target/$(TARGET)release/libpgp_ffi.dll" release/lib/librpgp.dll
-    cp "target/$(TARGET)/release/librpgp.h" release/include/
-    cp "target/$(TARGET)/release/pkgconfig/rpgp.pc" release/lib/pkgconfig
+    cp "target/${TARGET}release/libpgp_ffi.dll" release/lib/librpgp.dll
+    cp "target/${TARGET}/release/librpgp.h" release/include/
+    cp "target/${TARGET}/release/pkgconfig/rpgp.pc" release/lib/pkgconfig
 
 elif [[ $TARGET = *"darwin"* ]]; then
     cargo build --release --features nightly -p pgp_ffi --target $TARGET
     mkdir -p release/lib/pkgconfig release/include
-    cp "target/$(TARGET)release/libpgp_ffi.dylib" release/lib/librpgp.dylib
-    cp "target/$(TARGET)/release/librpgp.h" release/include/
-    cp "target/$(TARGET)/release/pkgconfig/rpgp.pc" release/lib/pkgconfig
+    cp "target/${TARGET}release/libpgp_ffi.dylib" release/lib/librpgp.dylib
+    cp "target/${TARGET}/release/librpgp.h" release/include/
+    cp "target/${TARGET}/release/pkgconfig/rpgp.pc" release/lib/pkgconfig
 else
     # nix systems
     RUSTFLAGS="-C codegen-units=1" cross build --release --features nightly -p pgp_ffi --target $TARGET
     mkdir -p release/lib/pkgconfig release/include
-    cp "target/$(TARGET)release/libpgp_ffi.so" release/lib/librpgp.so
-    cp "target/$(TARGET)/release/librpgp.h" release/include/
-    cp "target/$(TARGET)/release/pkgconfig/rpgp.pc" release/lib/pkgconfig
+    cp "target/${TARGET}release/libpgp_ffi.so" release/lib/librpgp.so
+    cp "target/${TARGET}/release/librpgp.h" release/include/
+    cp "target/${TARGET}/release/pkgconfig/rpgp.pc" release/lib/pkgconfig
 fi
 
 
 TAG=`git describe --tags`
 
-tar cvzf "librpgp-$(TAG)-$(TARGET).tar.gz" release
+tar cvzf "librpgp-${TAG}-${TARGET}.tar.gz" release
 
 # TODO: upload everything in release
