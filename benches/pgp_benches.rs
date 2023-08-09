e156f17c16f9acf0a3346ddb864a1ffbb4611621
Andreas Tzionis <andy0n7@gmail.com>
2023-08-09 14:09:54 +0300

    fixed clippy and fmt, added error propagation

--- benches/pgp_benches.rs
+++ benches/pgp_benches.rs
@@ -1,12 +1,12 @@
-#![feature(test)]
+// #![feature(test)]
 
 extern crate pgp;
-extern crate test;
-#[macro_use]
+// extern crate test;
+// #[macro_use]
 extern crate smallvec;
 
 #[cfg(feature = "profile")]
 extern crate gperftools;
 
-mod key;
-mod message;
+// mod key;
+// mod message;
