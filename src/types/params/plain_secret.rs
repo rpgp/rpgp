f79a732bf67fff0b1ddb1608df4b051ab9e585fc
Daniel Kahn Gillmor <dkg@fifthhorseman.net>
2024-10-22 17:30:51 -0400

    Gate x448 work behind an "unstable-curve448" feature.
    The x448 crate hasn't had a release in over 2 years, while several of
    its dependencies are still rather unstable (in particular,
    ed448-goldilocks and rand_core have had at least one
    semver-incompatible bump since the x448 release, and ed448-goldilocks
    itself probably needs another release as well, since there are many
    useful cleanup changes pending there).
    
    This makes me think that x448 isn't quite ready for prime time in the
    existing rust ecosystem.  Given that ed448 itself is also not
    supported yet, it seems like any work with Curve448 might be premature
    to make available by default.
    
    This patch proposes to put the x448 code behind a feature named
    "unstable-curve448".  It will also make it easier to land the rest of
    the RFC 9580 work in debian, which is currently struggling with the
    unmaintained dependency chain here.

--- src/types/params/plain_secret.rs
+++ src/types/params/plain_secret.rs
@@ -38,6 +38,7 @@ pub enum PlainSecretParams {
     EdDSALegacy(#[debug("..")] Mpi),
     Ed25519(#[debug("..")] [u8; 32]),
     X25519(#[debug("..")] [u8; 32]),
+    #[cfg(feature = "x448")]
     X448(#[debug("..")] [u8; 56]),
 }
 
@@ -60,6 +61,7 @@ pub enum PlainSecretParamsRef<'a> {
     EdDSALegacy(#[debug("..")] MpiRef<'a>),
     Ed25519(#[debug("..")] &'a [u8; 32]),
     X25519(#[debug("..")] &'a [u8; 32]),
+    #[cfg(feature = "x448")]
     X448(#[debug("..")] &'a [u8; 56]),
 }
 
@@ -79,6 +81,7 @@ impl<'a> PlainSecretParamsRef<'a> {
             PlainSecretParamsRef::EdDSALegacy(v) => PlainSecretParams::EdDSALegacy((*v).to_owned()),
             PlainSecretParamsRef::Ed25519(s) => PlainSecretParams::Ed25519((*s).to_owned()),
             PlainSecretParamsRef::X25519(s) => PlainSecretParams::X25519((*s).to_owned()),
+            #[cfg(feature = "x448")]
             PlainSecretParamsRef::X448(s) => PlainSecretParams::X448((*s).to_owned()),
         }
     }
@@ -116,6 +119,7 @@ impl<'a> PlainSecretParamsRef<'a> {
             PlainSecretParamsRef::X25519(s) => {
                 writer.write_all(&s[..])?;
             }
+            #[cfg(feature = "x448")]
             PlainSecretParamsRef::X448(s) => {
                 writer.write_all(&s[..])?;
             }
@@ -254,6 +258,7 @@ impl<'a> PlainSecretParamsRef<'a> {
                     secret: **d,
                 }))
             }
+            #[cfg(feature = "x448")]
             PlainSecretParamsRef::X448(d) => {
                 Ok(SecretKeyRepr::X448(crate::crypto::x448::SecretKey {
                     secret: **d,
@@ -343,6 +348,7 @@ impl PlainSecretParams {
             PlainSecretParams::EdDSALegacy(v) => PlainSecretParamsRef::EdDSALegacy(v.as_ref()),
             PlainSecretParams::Ed25519(s) => PlainSecretParamsRef::Ed25519(s),
             PlainSecretParams::X25519(s) => PlainSecretParamsRef::X25519(s),
+            #[cfg(feature = "x448")]
             PlainSecretParams::X448(s) => PlainSecretParamsRef::X448(s),
         }
     }
@@ -492,6 +498,7 @@ fn parse_secret_params(
             let (i, s) = nom::bytes::complete::take(32u8)(i)?;
             Ok((i, PlainSecretParams::X25519(s.try_into().expect("32"))))
         }
+        #[cfg(feature = "x448")]
         PublicKeyAlgorithm::X448 => {
             let (i, s) = nom::bytes::complete::take(56u8)(i)?;
             Ok((i, PlainSecretParams::X448(s.try_into().expect("56"))))
