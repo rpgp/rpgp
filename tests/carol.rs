2b08dceb8dd833c592c28c65585ec0a05fb9446d
dignifiedquire <me@dignifiedquire.com>
2025-02-08 11:35:26 +0100

    test: add failing test for subpacket key length
    Ref https://github.com/rpgp/rpgp/pull/463

--- tests/carol.rs
+++ tests/carol.rs
@@ -29,11 +29,10 @@ fn load_carol_pub() {
 
     let original_key = std::fs::read_to_string("tests/carol.pub.asc").unwrap();
 
-    let (sec_key, headers) =
-        pgp::composed::SignedPublicKey::from_armor_single(original_key.as_bytes())
-            .expect("parsing");
+    let (key, headers) = pgp::composed::SignedPublicKey::from_armor_single(original_key.as_bytes())
+        .expect("parsing");
 
-    let serialized_key = sec_key
+    let serialized_key = key
         .to_armored_string(ArmorOptions {
             headers: Some(&headers),
             ..Default::default()
---
+++ tests/evn.rs
@@ -0,0 +1,30 @@
+#[macro_use]
+extern crate pretty_assertions;
+
+use pgp::{ArmorOptions, Deserializable};
+
+/// "evn.cert" is a real world certificate with some unusual properties:
+/// For one thing, it encodes subpacket length in the 5 byte format.
+///
+/// When hashing a signature, the exact subpacket length encoding must be preserved,
+/// otherwise the signature doesn't verify correctly.
+#[test]
+fn load_evn_pub() {
+    let _ = pretty_env_logger::try_init();
+
+    let original_key = std::fs::read_to_string("tests/evn.cert").unwrap();
+
+    let (key, headers) = pgp::composed::SignedPublicKey::from_armor_single(original_key.as_bytes())
+        .expect("parsing");
+
+    key.verify().expect("failed to verify");
+
+    let serialized_key = key
+        .to_armored_string(ArmorOptions {
+            headers: Some(&headers),
+            ..Default::default()
+        })
+        .expect("failed to serialize");
+
+    assert_eq!(original_key, serialized_key);
+}
