8371119e30859d8bb44fbfcb4a4a2a1ac838c446
Heiko Schaefer <59601023+hko-s@users.noreply.github.com>
2025-03-02 16:37:14 +0100

    CSF message that starts with a newline (#478)
    * test: csf message that starts with a newline
    
    * tweak header handling: the "hash headers" don't include the empty line
    
    * expect exactly one empty/whitespace line between header and armored data
    
    * should we consider this test vector wrong (and fix it)?
    
    according to https://www.rfc-editor.org/rfc/rfc9580.html#name-forming-ascii-armor
    ... do we need to be lenient and allow it, or do we tighten the parser and fix the test?
    
    * add assert for signature header
    
    * verify csf signature

--- tests/any_test.rs
+++ tests/any_test.rs
@@ -1,3 +1,7 @@
+use std::fs::File;
+
+use pgp::{Deserializable, SignedPublicKey};
+
 #[test]
 fn load_csf() {
     // Try to load a regular CSF message
@@ -14,4 +18,34 @@ fn load_empty_csf() {
 
     let (any, _) = pgp::Any::from_armor(empty_csf_msg.as_bytes()).expect("from_armor");
     assert!(matches![any, pgp::Any::Cleartext(_)]);
+}
+
+#[test]
+fn load_csf_starts_with_newline() {
+    // Load a CSF message with the message '\ntest\n'
+    let empty_csf_msg =
+        std::fs::read_to_string("tests/unit-tests/csf/starts-with-newline.csf").unwrap();
+
+    let (any, headers) = pgp::Any::from_armor(empty_csf_msg.as_bytes()).expect("from_armor");
+
+    // The header in the SIGNATURE block is empty
+    assert!(headers.is_empty());
+
+    match any {
+        pgp::Any::Cleartext(csm) => {
+            // Message payload with line endings normalized (CI on windows produces CRLF line endings)
+            let payload = csm.text().replace("\r\n", "\n");
+
+            assert_eq!(payload, "\ntest\n");
+
+            // verify signature
+            let (pkey, _) = SignedPublicKey::from_armor_single(
+                File::open("./tests/unit-tests/csf/alice.pub.asc").unwrap(),
+            )
+            .unwrap();
+
+            csm.verify(&pkey).expect("verify ok");
+        }
+        _ => panic!("unexpected type"),
+    }
 }
