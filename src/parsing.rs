8c121cf891b9af661b057a7429d679c773c677a0
dignifiedquire <me@dignifiedquire.com>
2025-03-08 23:58:40 +0100

    remove dead code

--- src/parsing.rs
+++ src/parsing.rs
@@ -75,15 +75,6 @@ pub trait BufParsing: Buf + Sized {
         Ok(self.get_u8())
     }
 
-    fn read_be_u16(&mut self) -> Result<u16, Error> {
-        self.ensure_remaining(2).map_err(|e| Error::TooShort {
-            typ: Typ::U16Be,
-            source: e,
-            context: "todo",
-        })?;
-        Ok(self.get_u16())
-    }
-
     fn read_le_u16(&mut self) -> Result<u16, Error> {
         self.ensure_remaining(2).map_err(|e| Error::TooShort {
             typ: Typ::U16Le,
@@ -91,35 +82,6 @@ pub trait BufParsing: Buf + Sized {
             context: "todo",
         })?;
         Ok(self.get_u16_le())
-    }
-
-    fn read_be_u32(&mut self) -> Result<u32, Error> {
-        self.ensure_remaining(4).map_err(|e| Error::TooShort {
-            typ: Typ::U32Be,
-            source: e,
-            context: "todo",
-        })?;
-        Ok(self.get_u32())
-    }
-
-    fn read_array<const C: usize>(&mut self) -> Result<[u8; C], Error> {
-        self.ensure_remaining(C).map_err(|e| Error::TooShort {
-            typ: Typ::Array(C),
-            source: e,
-            context: "todo",
-        })?;
-        let mut arr = [0u8; C];
-        self.copy_to_slice(&mut arr);
-        Ok(arr)
-    }
-
-    fn read_take(&mut self, size: usize) -> Result<Bytes, Error> {
-        self.ensure_remaining(size).map_err(|e| Error::TooShort {
-            typ: Typ::Take(size),
-            source: e,
-            context: "todo",
-        })?;
-        Ok(self.copy_to_bytes(size))
     }
 
     fn rest(&mut self) -> Bytes {
@@ -136,25 +98,6 @@ pub trait BufParsing: Buf + Sized {
             });
         }
 
-        Ok(())
-    }
-
-    fn read_tag(&mut self, tag: &[u8]) -> Result<(), Error> {
-        self.ensure_remaining(tag.len())
-            .map_err(|e| Error::TooShort {
-                typ: Typ::Tag(tag.to_vec()),
-                source: e,
-                context: "todo",
-            })?;
-        let read = self.copy_to_bytes(tag.len());
-        if tag != read {
-            return Err(Error::TagMissmatch {
-                context: "todo",
-                expected: tag.to_vec(),
-                found: read,
-                backtrace: snafu::GenerateImplicitData::generate(),
-            });
-        }
         Ok(())
     }
 }
