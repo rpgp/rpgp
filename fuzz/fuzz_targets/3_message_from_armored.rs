18371269344a3d9064dc91d41c7fba7708ce671f
Heiko Schaefer <heiko@schaefer.name>
2025-03-29 19:44:44 +0100

    test: port fuzz tests

--- fuzz/fuzz_targets/3_message_from_armored.rs
+++ fuzz/fuzz_targets/3_message_from_armored.rs
@@ -1,7 +1,8 @@
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
-use pgp::composed::{Deserializable, Message};
+use pgp::composed::Message;
+use pgp::types::Password;
 
 // build message from armored data, delivered here in raw bytes to simulate file
 fuzz_target!(|data: &[u8]| {
@@ -19,13 +20,13 @@ fuzz_target!(|data: &[u8]| {
             let (message, _) = message_tuple;
             // FUZZER RESULT this can panic on some inputs
             // finding RPG-19 in ROS report 2024, fixed with 0.14.1
-            let _ = message.decrypt_with_password(|| "bogus_password".into());
+            let _ = message.decrypt_with_password(&Password::from("bogus_password"));
 
-            let _ = message.clone().is_one_pass_signed();
-            let _ = message.clone().is_literal();
-            let _ = message.clone().get_literal();
+            let _ = Message::from_armor(data).unwrap().0.is_one_pass_signed();
+            let _ = Message::from_armor(data).unwrap().0.is_literal();
+            let _ = Message::from_armor(data).unwrap().0.as_data_string();
             // attempts decompression for some message types
-            let _ = message.clone().get_content();
+            let _ = Message::from_armor(data).unwrap().0.get_content();
         }
     }
 });
