95cc8b7c1c44a8e945fe6db8ee25379a169eed53
dignifiedquire <me@dignifiedquire.com>
2025-04-15 13:48:52 +0200

    test: deduplicate some code

--- tests/pqc.rs
+++ tests/pqc.rs
@@ -12,63 +12,99 @@ use rand_chacha::ChaCha8Rng;
 use smallvec::smallvec;
 use testresult::TestResult;
 
+enum TestCase {
+    TransferableSecretKey {
+        source: &'static str,
+        primary_key_fp: &'static str,
+        primary_key_alg: PublicKeyAlgorithm,
+        sub_keys: Vec<(&'static str, PublicKeyAlgorithm)>,
+    },
+    TransferablePublicKey {
+        source: &'static str,
+        primary_key_alg: PublicKeyAlgorithm,
+        sub_keys: Vec<PublicKeyAlgorithm>,
+    },
+}
+
+impl TestCase {
+    fn test(&self) -> TestResult {
+        match self {
+            Self::TransferableSecretKey {
+                source,
+                primary_key_fp,
+                primary_key_alg,
+                sub_keys,
+            } => {
+                let (key, _) = SignedSecretKey::from_armor_file(source)?;
+
+                assert_eq!(key.primary_key.algorithm(), *primary_key_alg);
+                assert_eq!(&key.primary_key.fingerprint().to_string(), primary_key_fp);
+
+                for ((fp, alg), sub_key) in sub_keys.iter().zip(key.secret_subkeys.iter()) {
+                    assert_eq!(sub_key.algorithm(), *alg,);
+                    assert_eq!(sub_key.fingerprint().to_string(), *fp,);
+                }
+                assert_eq!(key.secret_subkeys.len(), sub_keys.len());
+
+                key.verify()?;
+
+                Ok(())
+            }
+            Self::TransferablePublicKey {
+                source,
+                primary_key_alg,
+                sub_keys,
+            } => {
+                let (key, _) = SignedPublicKey::from_armor_file(source)?;
+
+                assert_eq!(key.primary_key.algorithm(), *primary_key_alg);
+
+                for (alg, sub_key) in sub_keys.iter().zip(key.public_subkeys.iter()) {
+                    assert_eq!(sub_key.algorithm(), *alg,);
+                }
+                assert_eq!(key.public_subkeys.len(), sub_keys.len());
+
+                key.verify()?;
+
+                Ok(())
+            }
+        }
+    }
+}
+
 #[test]
 fn test_a_1_1_transferable_secret_key() -> TestResult {
     // Sample v6 Ed25519 with ML-KEM-768+X25519 Data
 
-    let _ = pretty_env_logger::try_init();
-
-    let (key, _) = SignedSecretKey::from_armor_file("./tests/pqc/v6-eddsa-sample-sk.asc")?;
-
-    assert_eq!(key.primary_key.algorithm(), PublicKeyAlgorithm::Ed25519);
-    assert_eq!(
-        key.primary_key.fingerprint().to_string(),
-        "2357faea8775f69acb11183f81b765cc30db7daf2768827babe202a16d07d4aa"
-    );
-
-    assert_eq!(
-        key.secret_subkeys[0].algorithm(),
-        PublicKeyAlgorithm::X25519
-    );
-    assert_eq!(
-        key.secret_subkeys[0].fingerprint().to_string(),
-        "fe0f1b20e62a56caacc4d68f32e5a0a3c1e7a69a7d13541fa1761a3933b5b8cf"
-    );
-
-    assert_eq!(
-        key.secret_subkeys[1].algorithm(),
-        PublicKeyAlgorithm::MlKem768X25519
-    );
-    assert_eq!(
-        key.secret_subkeys[1].fingerprint().to_string(),
-        "23eee71a76bc1eab20017a2ba4af492136ec6e6296ed60128b2223273bcb4d2c"
-    );
-
-    key.verify()?;
-
-    Ok(())
+    TestCase::TransferableSecretKey {
+        source: "./tests/pqc/v6-eddsa-sample-sk.asc",
+        primary_key_alg: PublicKeyAlgorithm::Ed25519,
+        primary_key_fp: "2357faea8775f69acb11183f81b765cc30db7daf2768827babe202a16d07d4aa",
+        sub_keys: vec![
+            (
+                "fe0f1b20e62a56caacc4d68f32e5a0a3c1e7a69a7d13541fa1761a3933b5b8cf",
+                PublicKeyAlgorithm::X25519,
+            ),
+            (
+                "23eee71a76bc1eab20017a2ba4af492136ec6e6296ed60128b2223273bcb4d2c",
+                PublicKeyAlgorithm::MlKem768X25519,
+            ),
+        ],
+    }
+    .test()
 }
 
 #[test]
 fn test_a_1_2_transferable_public_key() -> TestResult {
-    let _ = pretty_env_logger::try_init();
-
-    let (key, _) = SignedPublicKey::from_armor_file("./tests/pqc/v6-eddsa-sample-pk.asc")?;
-
-    assert_eq!(key.primary_key.algorithm(), PublicKeyAlgorithm::Ed25519);
-
-    assert_eq!(
-        key.public_subkeys[0].algorithm(),
-        PublicKeyAlgorithm::X25519
-    );
-    assert_eq!(
-        key.public_subkeys[1].algorithm(),
-        PublicKeyAlgorithm::MlKem768X25519
-    );
-
-    key.verify()?;
-
-    Ok(())
+    TestCase::TransferablePublicKey {
+        source: "./tests/pqc/v6-eddsa-sample-pk.asc",
+        primary_key_alg: PublicKeyAlgorithm::Ed25519,
+        sub_keys: vec![
+            PublicKeyAlgorithm::X25519,
+            PublicKeyAlgorithm::MlKem768X25519,
+        ],
+    }
+    .test()
 }
 
 #[test]
@@ -122,59 +158,35 @@ fn test_a_1_3_signed_encrypted() -> TestResult {
 fn test_a_2_1_transferable_secret_key() -> TestResult {
     // Sample v4 Ed25519 with ML-KEM-768+X25519 Data
 
-    let _ = pretty_env_logger::try_init();
-
-    let (key, _) = SignedSecretKey::from_armor_file("./tests/pqc/v4-eddsa-sample-sk.asc")?;
-
-    assert_eq!(key.primary_key.algorithm(), PublicKeyAlgorithm::Ed25519);
-    assert_eq!(
-        key.primary_key.fingerprint().to_string(),
-        "bee82527bae0f931a3195628a3687fdca62e4844"
-    );
-
-    assert_eq!(
-        key.secret_subkeys[0].algorithm(),
-        PublicKeyAlgorithm::X25519
-    );
-    assert_eq!(
-        key.secret_subkeys[0].fingerprint().to_string(),
-        "3e6a6bd51614ff3810ad2256ada71a07c0afbd7d"
-    );
-
-    assert_eq!(
-        key.secret_subkeys[1].algorithm(),
-        PublicKeyAlgorithm::MlKem768X25519
-    );
-    assert_eq!(
-        key.secret_subkeys[1].fingerprint().to_string(),
-        "3c5e54c7de276f3e308e7da8c5bcde48f991e7c8"
-    );
-
-    key.verify()?;
-
-    Ok(())
+    TestCase::TransferableSecretKey {
+        source: "./tests/pqc/v4-eddsa-sample-sk.asc",
+        primary_key_alg: PublicKeyAlgorithm::Ed25519,
+        primary_key_fp: "bee82527bae0f931a3195628a3687fdca62e4844",
+        sub_keys: vec![
+            (
+                "3e6a6bd51614ff3810ad2256ada71a07c0afbd7d",
+                PublicKeyAlgorithm::X25519,
+            ),
+            (
+                "3c5e54c7de276f3e308e7da8c5bcde48f991e7c8",
+                PublicKeyAlgorithm::MlKem768X25519,
+            ),
+        ],
+    }
+    .test()
 }
 
 #[test]
 fn test_a_2_2_transferable_public_key() -> TestResult {
-    let _ = pretty_env_logger::try_init();
-
-    let (key, _) = SignedPublicKey::from_armor_file("./tests/pqc/v4-eddsa-sample-pk.asc")?;
-
-    assert_eq!(key.primary_key.algorithm(), PublicKeyAlgorithm::Ed25519);
-
-    assert_eq!(
-        key.public_subkeys[0].algorithm(),
-        PublicKeyAlgorithm::X25519
-    );
-    assert_eq!(
-        key.public_subkeys[1].algorithm(),
-        PublicKeyAlgorithm::MlKem768X25519
-    );
-
-    key.verify()?;
-
-    Ok(())
+    TestCase::TransferablePublicKey {
+        source: "./tests/pqc/v4-eddsa-sample-pk.asc",
+        primary_key_alg: PublicKeyAlgorithm::Ed25519,
+        sub_keys: vec![
+            PublicKeyAlgorithm::X25519,
+            PublicKeyAlgorithm::MlKem768X25519,
+        ],
+    }
+    .test()
 }
 
 #[test]
@@ -298,52 +310,26 @@ fn test_ml_kem_1024_x448() -> TestResult {
 fn test_a_3_1_transferable_secret_key() -> TestResult {
     // Sample ML-DSA-65+Ed25519 with ML-KEM-768+X25519 Data
 
-    let _ = pretty_env_logger::try_init();
-
-    let (key, _) = SignedSecretKey::from_armor_file("./tests/pqc/v6-mldsa-65-sample-sk.asc")?;
-
-    assert_eq!(
-        key.primary_key.algorithm(),
-        PublicKeyAlgorithm::MlDsa65Ed25519
-    );
-    assert_eq!(
-        key.primary_key.fingerprint().to_string(),
-        "42120bfb467bf42c8a3eecb7fd38a8ba426ae95d916f9e77c3fd3f3955e1627d"
-    );
-
-    assert_eq!(
-        key.secret_subkeys[0].algorithm(),
-        PublicKeyAlgorithm::MlKem768X25519
-    );
-    assert_eq!(
-        key.secret_subkeys[0].fingerprint().to_string(),
-        "8333c14b27fd556d29b18141811531452dd88c23a1c09e92561521014c1cc460"
-    );
-
-    key.verify()?;
-
-    Ok(())
+    TestCase::TransferableSecretKey {
+        source: "./tests/pqc/v6-mldsa-65-sample-sk.asc",
+        primary_key_alg: PublicKeyAlgorithm::MlDsa65Ed25519,
+        primary_key_fp: "42120bfb467bf42c8a3eecb7fd38a8ba426ae95d916f9e77c3fd3f3955e1627d",
+        sub_keys: vec![(
+            "8333c14b27fd556d29b18141811531452dd88c23a1c09e92561521014c1cc460",
+            PublicKeyAlgorithm::MlKem768X25519,
+        )],
+    }
+    .test()
 }
 
 #[test]
 fn test_a_3_2_transferable_public_key() -> TestResult {
-    let _ = pretty_env_logger::try_init();
-
-    let (key, _) = SignedPublicKey::from_armor_file("./tests/pqc/v6-mldsa-65-sample-pk.asc")?;
-
-    assert_eq!(
-        key.primary_key.algorithm(),
-        PublicKeyAlgorithm::MlDsa65Ed25519
-    );
-
-    assert_eq!(
-        key.public_subkeys[0].algorithm(),
-        PublicKeyAlgorithm::MlKem768X25519
-    );
-
-    key.verify()?;
-
-    Ok(())
+    TestCase::TransferablePublicKey {
+        source: "./tests/pqc/v6-mldsa-65-sample-pk.asc",
+        primary_key_alg: PublicKeyAlgorithm::MlDsa65Ed25519,
+        sub_keys: vec![PublicKeyAlgorithm::MlKem768X25519],
+    }
+    .test()
 }
 
 #[test]
@@ -416,52 +402,26 @@ fn test_a_3_4_detached_signature() -> TestResult {
 fn test_a_4_1_transferable_secret_key() -> TestResult {
     // Sample ML-DSA-87+Ed448 with ML-KEM-1024+X448 Data
 
-    let _ = pretty_env_logger::try_init();
-
-    let (key, _) = SignedSecretKey::from_armor_file("./tests/pqc/v6-mldsa-87-sample-sk.asc")?;
-
-    assert_eq!(
-        key.primary_key.algorithm(),
-        PublicKeyAlgorithm::MlDsa87Ed448
-    );
-    assert_eq!(
-        key.primary_key.fingerprint().to_string(),
-        "4141f9deb6ee8c3f8484c3e0d0f41796da5c6b8e6994145e3a335f557cf544c3"
-    );
-
-    assert_eq!(
-        key.secret_subkeys[0].algorithm(),
-        PublicKeyAlgorithm::MlKem1024X448
-    );
-    assert_eq!(
-        key.secret_subkeys[0].fingerprint().to_string(),
-        "8cc1fdaed98c2f3b0601eab83fe96e06a44d234bbe61d9b04c1e81c4f66d2080"
-    );
-
-    key.verify()?;
-
-    Ok(())
+    TestCase::TransferableSecretKey {
+        source: "./tests/pqc/v6-mldsa-87-sample-sk.asc",
+        primary_key_alg: PublicKeyAlgorithm::MlDsa87Ed448,
+        primary_key_fp: "4141f9deb6ee8c3f8484c3e0d0f41796da5c6b8e6994145e3a335f557cf544c3",
+        sub_keys: vec![(
+            "8cc1fdaed98c2f3b0601eab83fe96e06a44d234bbe61d9b04c1e81c4f66d2080",
+            PublicKeyAlgorithm::MlKem1024X448,
+        )],
+    }
+    .test()
 }
 
 #[test]
 fn test_a_4_2_transferable_public_key() -> TestResult {
-    let _ = pretty_env_logger::try_init();
-
-    let (key, _) = SignedPublicKey::from_armor_file("./tests/pqc/v6-mldsa-87-sample-pk.asc")?;
-
-    assert_eq!(
-        key.primary_key.algorithm(),
-        PublicKeyAlgorithm::MlDsa87Ed448
-    );
-
-    assert_eq!(
-        key.public_subkeys[0].algorithm(),
-        PublicKeyAlgorithm::MlKem1024X448
-    );
-
-    key.verify()?;
-
-    Ok(())
+    TestCase::TransferablePublicKey {
+        source: "./tests/pqc/v6-mldsa-87-sample-pk.asc",
+        primary_key_alg: PublicKeyAlgorithm::MlDsa87Ed448,
+        sub_keys: vec![PublicKeyAlgorithm::MlKem1024X448],
+    }
+    .test()
 }
 
 #[test]
@@ -534,54 +494,26 @@ fn test_a_4_4_detached_signature() -> TestResult {
 fn test_a_5_1_transferable_secret_key() -> TestResult {
     // Sample SLH-DSA-128s with ML-KEM-768+X25519 Data
 
-    let _ = pretty_env_logger::try_init();
-
-    let (key, _) = SignedSecretKey::from_armor_file("./tests/pqc/v6-slhdsa-128s-sample-sk.asc")?;
-
-    assert_eq!(
-        key.primary_key.algorithm(),
-        PublicKeyAlgorithm::SlhDsaShake128s
-    );
-    assert_eq!(
-        key.primary_key.fingerprint().to_string(),
-        "e761d4ec762a5f9c35f72b0c8a030c184b903c35459e74b25341b245819ab3fe"
-    );
-
-    assert_eq!(
-        key.secret_subkeys[0].algorithm(),
-        PublicKeyAlgorithm::MlKem768X25519
-    );
-    assert_eq!(
-        key.secret_subkeys[0].fingerprint().to_string(),
-        "1090ff914d4fb0a40eb3354aeec8575609f0f72e6ad881f54e94932cd78227f6"
-    );
-    assert_eq!(key.secret_subkeys.len(), 1);
-
-    key.verify()?;
-
-    Ok(())
+    TestCase::TransferableSecretKey {
+        source: "./tests/pqc/v6-slhdsa-128s-sample-sk.asc",
+        primary_key_alg: PublicKeyAlgorithm::SlhDsaShake128s,
+        primary_key_fp: "e761d4ec762a5f9c35f72b0c8a030c184b903c35459e74b25341b245819ab3fe",
+        sub_keys: vec![(
+            "1090ff914d4fb0a40eb3354aeec8575609f0f72e6ad881f54e94932cd78227f6",
+            PublicKeyAlgorithm::MlKem768X25519,
+        )],
+    }
+    .test()
 }
 
 #[test]
 fn test_a_5_2_transferable_public_key() -> TestResult {
-    let _ = pretty_env_logger::try_init();
-
-    let (key, _) = SignedPublicKey::from_armor_file("./tests/pqc/v6-slhdsa-128s-sample-pk.asc")?;
-
-    assert_eq!(
-        key.primary_key.algorithm(),
-        PublicKeyAlgorithm::SlhDsaShake128s
-    );
-
-    assert_eq!(
-        key.public_subkeys[0].algorithm(),
-        PublicKeyAlgorithm::MlKem768X25519
-    );
-    assert_eq!(key.public_subkeys.len(), 1);
-
-    key.verify()?;
-
-    Ok(())
+    TestCase::TransferablePublicKey {
+        source: "./tests/pqc/v6-slhdsa-128s-sample-pk.asc",
+        primary_key_alg: PublicKeyAlgorithm::SlhDsaShake128s,
+        sub_keys: vec![PublicKeyAlgorithm::MlKem768X25519],
+    }
+    .test()
 }
 
 #[test]
@@ -657,55 +589,26 @@ fn test_a_5_4_detached_signature() -> TestResult {
 fn test_a_6_1_transferable_secret_key() -> TestResult {
     // Sample SLH-DSA-128f with ML-KEM-768+X25519 Data
 
-    let _ = pretty_env_logger::try_init();
-
-    let (key, _) = SignedSecretKey::from_armor_file("./tests/pqc/v6-slhdsa-128f-sample-sk.asc")?;
-
-    assert_eq!(
-        key.primary_key.algorithm(),
-        PublicKeyAlgorithm::SlhDsaShake128f
-    );
-    assert_eq!(
-        key.primary_key.fingerprint().to_string(),
-        "7625d0725493f2a0c38080e3a3928016d73ec056e4cf54b1f93a1da7794e67ad"
-    );
-
-    assert_eq!(
-        key.secret_subkeys[0].algorithm(),
-        PublicKeyAlgorithm::MlKem768X25519
-    );
-    assert_eq!(
-        key.secret_subkeys[0].fingerprint().to_string(),
-        "cea501a4831757a33b9fa03973b81656cf2ecac6f705daf1647e1f7190366ca6"
-    );
-
-    assert_eq!(key.secret_subkeys.len(), 1);
-
-    key.verify()?;
-
-    Ok(())
+    TestCase::TransferableSecretKey {
+        source: "./tests/pqc/v6-slhdsa-128f-sample-sk.asc",
+        primary_key_alg: PublicKeyAlgorithm::SlhDsaShake128f,
+        primary_key_fp: "7625d0725493f2a0c38080e3a3928016d73ec056e4cf54b1f93a1da7794e67ad",
+        sub_keys: vec![(
+            "cea501a4831757a33b9fa03973b81656cf2ecac6f705daf1647e1f7190366ca6",
+            PublicKeyAlgorithm::MlKem768X25519,
+        )],
+    }
+    .test()
 }
 
 #[test]
 fn test_a_6_2_transferable_public_key() -> TestResult {
-    let _ = pretty_env_logger::try_init();
-
-    let (key, _) = SignedPublicKey::from_armor_file("./tests/pqc/v6-slhdsa-128f-sample-pk.asc")?;
-
-    assert_eq!(
-        key.primary_key.algorithm(),
-        PublicKeyAlgorithm::SlhDsaShake128f
-    );
-
-    assert_eq!(
-        key.public_subkeys[0].algorithm(),
-        PublicKeyAlgorithm::MlKem768X25519
-    );
-    assert_eq!(key.public_subkeys.len(), 1);
-
-    key.verify()?;
-
-    Ok(())
+    TestCase::TransferablePublicKey {
+        source: "./tests/pqc/v6-slhdsa-128f-sample-pk.asc",
+        primary_key_alg: PublicKeyAlgorithm::SlhDsaShake128f,
+        sub_keys: vec![PublicKeyAlgorithm::MlKem768X25519],
+    }
+    .test()
 }
 
 #[test]
@@ -733,55 +636,26 @@ fn test_a_6_3_detached_signature() -> TestResult {
 fn test_a_7_1_transferable_secret_key() -> TestResult {
     // Sample SLH-DSA-256s with ML-KEM-1024+X448 Data
 
-    let _ = pretty_env_logger::try_init();
-
-    let (key, _) = SignedSecretKey::from_armor_file("./tests/pqc/v6-slhdsa-256s-sample-sk.asc")?;
-
-    assert_eq!(
-        key.primary_key.algorithm(),
-        PublicKeyAlgorithm::SlhDsaShake256s
-    );
-    assert_eq!(
-        key.primary_key.fingerprint().to_string(),
-        "eb55807530d02e475e5a6f403fec5ff9c60b078395fab4c9a862ec8c82a12a95"
-    );
-
-    assert_eq!(
-        key.secret_subkeys[0].algorithm(),
-        PublicKeyAlgorithm::MlKem1024X448
-    );
-    assert_eq!(
-        key.secret_subkeys[0].fingerprint().to_string(),
-        "6e8bbbed8d24472510941bf18639f7f799f86e8d8f3a8f49694e5687885388c1"
-    );
-
-    assert_eq!(key.secret_subkeys.len(), 1);
-
-    key.verify()?;
-
-    Ok(())
+    TestCase::TransferableSecretKey {
+        source: "./tests/pqc/v6-slhdsa-256s-sample-sk.asc",
+        primary_key_alg: PublicKeyAlgorithm::SlhDsaShake256s,
+        primary_key_fp: "eb55807530d02e475e5a6f403fec5ff9c60b078395fab4c9a862ec8c82a12a95",
+        sub_keys: vec![(
+            "6e8bbbed8d24472510941bf18639f7f799f86e8d8f3a8f49694e5687885388c1",
+            PublicKeyAlgorithm::MlKem1024X448,
+        )],
+    }
+    .test()
 }
 
 #[test]
 fn test_a_7_2_transferable_public_key() -> TestResult {
-    let _ = pretty_env_logger::try_init();
-
-    let (key, _) = SignedPublicKey::from_armor_file("./tests/pqc/v6-slhdsa-256s-sample-pk.asc")?;
-
-    assert_eq!(
-        key.primary_key.algorithm(),
-        PublicKeyAlgorithm::SlhDsaShake256s
-    );
-
-    assert_eq!(
-        key.public_subkeys[0].algorithm(),
-        PublicKeyAlgorithm::MlKem1024X448
-    );
-    assert_eq!(key.public_subkeys.len(), 1);
-
-    key.verify()?;
-
-    Ok(())
+    TestCase::TransferablePublicKey {
+        source: "./tests/pqc/v6-slhdsa-256s-sample-pk.asc",
+        primary_key_alg: PublicKeyAlgorithm::SlhDsaShake256s,
+        sub_keys: vec![PublicKeyAlgorithm::MlKem1024X448],
+    }
+    .test()
 }
 
 #[test]
