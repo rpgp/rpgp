6a42b719bcc49895fa39499b8c3e3346ccc31068
Heiko Schaefer <heiko@schaefer.name>
2025-05-30 01:51:02 +0200

    test: parsing a message with a pqc pkesk

--- tests/message_test.rs
+++ tests/message_test.rs
@@ -988,3 +988,16 @@ fn fuzz_msg_reader() {
         let _ = Message::from_file(file).unwrap().as_data_vec();
     }
 }
+
+#[test]
+fn message_parsing_pqc_pkesk() {
+    // This is a message that is encrypted to one traditional PKESK and one PQC PKESK
+    let (message, _) = Message::from_armor_file("./tests/message_pqc.asc").expect("ok");
+
+    let Message::Encrypted { esk, .. } = message else {
+        panic!("destructure encrypted message")
+    };
+
+    // We expect to find 1 or 2 esk, depending on if rPGP is compiled with support for pqc formats
+    assert!(!esk.is_empty());
+}
