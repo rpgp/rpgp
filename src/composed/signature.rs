5298f1a1da91d998e42336a2d3ec64e17860ebed
Heiko Schaefer <heiko@schaefer.name>
2025-08-07 11:49:30 +0200

    feat: implement signing in DetachedSignature

--- src/composed/signature.rs
+++ src/composed/signature.rs
@@ -1,16 +1,20 @@
-use std::iter::Peekable;
+use std::{io::Read, iter::Peekable};
+
+use aead::rand_core::CryptoRng;
+use rand::Rng;
 
 use crate::{
     armor,
-    composed::{ArmorOptions, Deserializable},
-    errors::{format_err, Result},
-    packet::{Packet, PacketTrait, Signature},
+    composed::{ArmorOptions, Deserializable, SubpacketConfig},
+    crypto::hash::HashAlgorithm,
+    errors::{bail, format_err, Result},
+    packet::{Packet, PacketTrait, Signature, SignatureConfig, SignatureType},
     ser::Serialize,
-    types::{PublicKeyTrait, Tag},
+    types::{KeyVersion, Password, PublicKeyTrait, SecretKeyTrait, Tag},
 };
 
 /// An OpenPGP data signature that occurs outside an OpenPGP Message,
-/// as a "detached signature":
+/// as a detached signature:
 ///
 /// <https://www.rfc-editor.org/rfc/rfc9580.html#detached-signatures>.
 ///
@@ -23,6 +27,122 @@ pub struct DetachedSignature {
 impl DetachedSignature {
     pub fn new(signature: Signature) -> Self {
         DetachedSignature { signature }
+    }
+
+    /// Create a detached data signature over `data`, with [SignatureType::Binary].
+    pub fn sign_binary_data<RNG: Rng + CryptoRng, R: Read>(
+        rng: RNG,
+        key: &impl SecretKeyTrait,
+        key_pw: &Password,
+        hash_algorithm: HashAlgorithm,
+        data: R,
+    ) -> Result<DetachedSignature> {
+        Self::sign_data(
+            rng,
+            SignatureType::Binary,
+            key,
+            key_pw,
+            hash_algorithm,
+            data,
+            SubpacketConfig::Default,
+        )
+    }
+
+    /// Create a detached data signature over `data`, with [SignatureType::Binary],
+    /// with explicit subpacket configuration.
+    ///
+    /// This gives callers full control of the hashed and unhashed subpacket areas.
+    pub fn sign_binary_data_with_subpackets<RNG: Rng + CryptoRng, R: Read>(
+        rng: RNG,
+        key: &impl SecretKeyTrait,
+        key_pw: &Password,
+        hash_algorithm: HashAlgorithm,
+        data: R,
+        subpackets: SubpacketConfig,
+    ) -> Result<DetachedSignature> {
+        Self::sign_data(
+            rng,
+            SignatureType::Binary,
+            key,
+            key_pw,
+            hash_algorithm,
+            data,
+            subpackets,
+        )
+    }
+
+    /// Create a detached data signature over `data`, with [SignatureType::Text].
+    ///
+    /// Using [SignatureType::Text] makes the signature stable against changes of line ending
+    /// encodings. The signature is not invalidated if the plaintext is e.g. changed between using
+    /// "LF" line endings or "CR+LF" line endings.
+    pub fn sign_text_data<RNG: Rng + CryptoRng, R: Read>(
+        rng: RNG,
+        key: &impl SecretKeyTrait,
+        key_pw: &Password,
+        hash_algorithm: HashAlgorithm,
+        data: R,
+    ) -> Result<DetachedSignature> {
+        Self::sign_data(
+            rng,
+            SignatureType::Text,
+            key,
+            key_pw,
+            hash_algorithm,
+            data,
+            SubpacketConfig::Default,
+        )
+    }
+
+    /// Create a detached data signature over `data`, with [SignatureType::Text],
+    /// with explicit subpacket configuration.
+    ///
+    /// This gives callers full control of the hashed and unhashed subpacket areas.
+    ///
+    /// Using [SignatureType::Text] makes the signature stable against changes of line ending
+    /// encodings. The signature is not invalidated if the plaintext is e.g. changed between using
+    /// "LF" line endings or "CR+LF" line endings.
+    pub fn sign_text_data_with_subpackets<RNG: Rng + CryptoRng, R: Read>(
+        rng: RNG,
+        key: &impl SecretKeyTrait,
+        key_pw: &Password,
+        hash_algorithm: HashAlgorithm,
+        data: R,
+        subpackets: SubpacketConfig,
+    ) -> Result<DetachedSignature> {
+        Self::sign_data(
+            rng,
+            SignatureType::Text,
+            key,
+            key_pw,
+            hash_algorithm,
+            data,
+            subpackets,
+        )
+    }
+
+    fn sign_data<RNG: Rng + CryptoRng, R: Read>(
+        rng: RNG,
+        typ: SignatureType,
+        key: &impl SecretKeyTrait,
+        key_pw: &Password,
+        hash_algorithm: HashAlgorithm,
+        data: R,
+        subpackets: SubpacketConfig,
+    ) -> Result<DetachedSignature> {
+        let mut config = match key.version() {
+            KeyVersion::V4 => SignatureConfig::v4(typ, key.algorithm(), hash_algorithm),
+            KeyVersion::V6 => SignatureConfig::v6(rng, typ, key.algorithm(), hash_algorithm)?,
+            v => bail!("unsupported key version: {:?}", v),
+        };
+
+        let (hashed, unhashed) = subpackets.to_subpackets(key)?;
+        config.hashed_subpackets = hashed;
+        config.unhashed_subpackets = unhashed;
+
+        let sig = config.sign(key, key_pw, data)?;
+
+        Ok(DetachedSignature::new(sig))
     }
 
     pub fn to_armored_writer(
@@ -104,5 +224,178 @@ fn next<I: Iterator<Item = Result<Packet>>>(
         },
         Some(Err(e)) => Some(Err(e)),
         None => None,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use chrono::SubsecRound;
+    use rand::SeedableRng;
+    use rand_chacha::ChaCha20Rng;
+
+    use crate::{
+        composed::{Deserializable, DetachedSignature, SignedSecretKey, SubpacketConfig},
+        crypto::hash::HashAlgorithm,
+        packet::{Subpacket, SubpacketData},
+        types::{KeyDetails, Password},
+    };
+
+    const PLAIN: &str = "hello world\r\n";
+    const PLAIN_LF: &str = "hello world\n";
+
+    #[test]
+    fn detached_signature_binary() {
+        let rng = ChaCha20Rng::seed_from_u64(1);
+
+        let (alice, _) =
+            SignedSecretKey::from_armor_file("./tests/autocrypt/alice@autocrypt.example.sec.asc")
+                .unwrap();
+
+        let sig = DetachedSignature::sign_binary_data(
+            rng,
+            &alice.primary_key,
+            &Password::empty(),
+            HashAlgorithm::Sha256,
+            PLAIN.as_bytes(),
+        )
+        .unwrap();
+
+        sig.verify(alice.primary_key.public_key(), PLAIN.as_bytes())
+            .expect("verify ok");
+
+        // inspect the signature for the expected subpacket structure
+        let cfg = sig.signature.config().unwrap();
+
+        assert_eq!(cfg.hashed_subpackets.len(), 2);
+        assert_eq!(cfg.unhashed_subpackets.len(), 1);
+
+        assert_eq!(sig.signature.issuer_fingerprint().len(), 1);
+        assert_eq!(sig.signature.issuer().len(), 1);
+
+        // differently normalized plaintext should not verify for SignatureType::Binary
+        sig.verify(alice.primary_key.public_key(), PLAIN_LF.as_bytes())
+            .expect_err("verify with unnormalized line ending not ok");
+    }
+
+    #[test]
+    fn detached_signature_text() {
+        let rng = ChaCha20Rng::seed_from_u64(1);
+
+        let (alice, _) =
+            SignedSecretKey::from_armor_file("./tests/autocrypt/alice@autocrypt.example.sec.asc")
+                .unwrap();
+
+        let sig = DetachedSignature::sign_text_data(
+            rng,
+            &alice.primary_key,
+            &Password::empty(),
+            HashAlgorithm::Sha256,
+            PLAIN.as_bytes(),
+        )
+        .unwrap();
+
+        sig.verify(alice.primary_key.public_key(), PLAIN.as_bytes())
+            .expect("verify ok");
+
+        // inspect the signature for the expected subpacket structure
+        let cfg = sig.signature.config().unwrap();
+
+        assert_eq!(cfg.hashed_subpackets.len(), 2);
+        assert_eq!(cfg.unhashed_subpackets.len(), 1);
+
+        assert_eq!(sig.signature.issuer_fingerprint().len(), 1);
+        assert_eq!(sig.signature.issuer().len(), 1);
+
+        // differently normalized plaintext should verify as ok for SignatureType::Text
+        sig.verify(alice.primary_key.public_key(), PLAIN_LF.as_bytes())
+            .expect("verify with unnormalized line ending is ok in text mode");
+    }
+
+    #[test]
+    fn detached_signature_binary_with_subpackets() {
+        let rng = ChaCha20Rng::seed_from_u64(1);
+
+        let (alice, _) =
+            SignedSecretKey::from_armor_file("./tests/autocrypt/alice@autocrypt.example.sec.asc")
+                .unwrap();
+
+        let hashed = vec![
+            Subpacket::regular(SubpacketData::IssuerFingerprint(alice.fingerprint())).unwrap(),
+            Subpacket::regular(SubpacketData::SignatureCreationTime(
+                chrono::Utc::now().trunc_subsecs(0),
+            ))
+            .unwrap(),
+            Subpacket::regular(SubpacketData::PolicyURI("foo".into())).unwrap(),
+        ];
+
+        let sig = DetachedSignature::sign_binary_data_with_subpackets(
+            rng,
+            &alice.primary_key,
+            &Password::empty(),
+            HashAlgorithm::Sha256,
+            PLAIN.as_bytes(),
+            SubpacketConfig::UserDefined {
+                hashed,
+                unhashed: vec![],
+            },
+        )
+        .unwrap();
+
+        sig.verify(alice.primary_key.public_key(), PLAIN.as_bytes())
+            .expect("verify ok");
+
+        // inspect the signature for the expected subpacket structure
+        let cfg = sig.signature.config().unwrap();
+
+        assert_eq!(cfg.hashed_subpackets.len(), 3);
+        assert!(cfg.unhashed_subpackets.is_empty());
+
+        // differently normalized plaintext should not verify for SignatureType::Binary
+        sig.verify(alice.primary_key.public_key(), PLAIN_LF.as_bytes())
+            .expect_err("verify with unnormalized line ending not ok");
+    }
+
+    #[test]
+    fn detached_signature_text_with_subpackets() {
+        let rng = ChaCha20Rng::seed_from_u64(1);
+
+        let (alice, _) =
+            SignedSecretKey::from_armor_file("./tests/autocrypt/alice@autocrypt.example.sec.asc")
+                .unwrap();
+
+        let hashed = vec![
+            Subpacket::regular(SubpacketData::IssuerFingerprint(alice.fingerprint())).unwrap(),
+            Subpacket::regular(SubpacketData::SignatureCreationTime(
+                chrono::Utc::now().trunc_subsecs(0),
+            ))
+            .unwrap(),
+            Subpacket::regular(SubpacketData::PolicyURI("foo".into())).unwrap(),
+        ];
+
+        let sig = DetachedSignature::sign_text_data_with_subpackets(
+            rng,
+            &alice.primary_key,
+            &Password::empty(),
+            HashAlgorithm::Sha256,
+            PLAIN.as_bytes(),
+            SubpacketConfig::UserDefined {
+                hashed,
+                unhashed: vec![],
+            },
+        )
+        .unwrap();
+
+        sig.verify(alice.primary_key.public_key(), PLAIN.as_bytes())
+            .expect("verify ok");
+
+        // inspect the signature for the expected subpacket structure
+        let cfg = sig.signature.config().unwrap();
+
+        assert_eq!(cfg.hashed_subpackets.len(), 3);
+        assert!(cfg.unhashed_subpackets.is_empty());
+
+        // differently normalized plaintext should verify as ok for SignatureType::Text
+        sig.verify(alice.primary_key.public_key(), PLAIN_LF.as_bytes())
+            .expect("verify with unnormalized line ending is ok in text mode");
     }
 }
