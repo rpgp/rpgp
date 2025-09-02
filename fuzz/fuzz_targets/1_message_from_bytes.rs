210ba97013dbdf9fad6d9896e1118b9d4c51dab2
Heiko Schaefer <heiko@schaefer.name>
2025-09-02 15:40:24 +0200

    test(fuzz): avoid redundant work

--- fuzz/fuzz_targets/1_message_from_bytes.rs
+++ fuzz/fuzz_targets/1_message_from_bytes.rs
@@ -1,8 +1,7 @@
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
-use pgp::composed::Message;
-use pgp::types::Password;
+use pgp::{composed::Message, types::Password};
 
 // build message from binary data
 fuzz_target!(|data: &[u8]| {
@@ -13,26 +12,23 @@ fuzz_target!(|data: &[u8]| {
     match message_res {
         Err(_) => return,
         Ok(message) => {
-            // not so interesting because it mostly tests external compression code?
-            // let _ = message.compress(CompressionAlgorithm::ZIP);
-            let _ = Message::from_bytes(data).unwrap().decompress();
+            let _ = message.is_one_pass_signed();
+            let _ = message.is_literal();
 
+            // -- Try to decrypt with a password and read the plaintext --
             // FUZZER RESULT this can panic on some inputs
             // finding RPG-19 in ROS report 2024, fixed with 0.14.1
             if let Ok(mut dec) = message.decrypt_with_password(&Password::from("bogus_password")) {
                 let _ = dec.as_data_vec();
             }
 
-            let _ = Message::from_bytes(data).unwrap().is_one_pass_signed();
-            let _ = Message::from_bytes(data).unwrap().is_literal();
+            // -- Try to just read the content of the message, as is --
             let _ = Message::from_bytes(data).unwrap().as_data_vec();
 
-            // attempts decompression for some message types
-            {
-                let msg = Message::from_bytes(data).unwrap();
-                if let Ok(mut msg) = msg.decompress() {
-                    let _ = msg.as_data_vec();
-                }
+            // -- Try to decompress and read the content --
+            let msg = Message::from_bytes(data).unwrap();
+            if let Ok(mut msg) = msg.decompress() {
+                let _ = msg.as_data_vec();
             }
         }
     }
