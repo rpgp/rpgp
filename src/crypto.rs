720a81013a9ceefaefc6158ed1344a65eeb96637
Heiko Schaefer <heiko@schaefer.name>
2025-11-22 23:42:03 +0100

    start support for draft-ietf-openpgp-persistent-symmetric-keys

--- src/crypto.rs
+++ src/crypto.rs
@@ -11,6 +11,7 @@ pub mod sym;
 
 // Encryption & Signing
 
+pub mod aead_key;
 pub mod dsa;
 pub mod ecdh;
 pub mod ecdsa;
--- src/packet/key.rs
+++ src/packet/key.rs
@@ -1,9 +1,11 @@
 mod public;
 mod secret;
+mod symmetric;
 
 pub(crate) use public::encrypt;
 
 pub use self::{
     public::{PubKeyInner, PublicKey, PublicSubkey},
     secret::{SecretKey, SecretSubkey},
+    symmetric::PersistentSymmetricKey,
 };
