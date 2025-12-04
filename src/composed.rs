4dd89107979e1becd735d51bd26698d98f489c3c
Heiko Schaefer <heiko@schaefer.name>
2025-12-04 23:22:45 +0100

    write example docs for packet level and composed level operations

--- src/composed.rs
+++ src/composed.rs
@@ -5,9 +5,10 @@
 //!
 //! Key generation is handled with [`SecretKeyParamsBuilder`].
 //!
-//! Once generated, keys are used as OpenPGP [Transferable Public Key] with [`SignedSecretKey`]
-//! or [Transferable Secret Key] with [`SignedPublicKey`].
-//! Those key objects can encrypt and decrypt messages, as well as issue and verify signatures.
+//! Once generated, composed keys are used as OpenPGP [Transferable Public Key] with
+//! [`SignedSecretKey`] or [Transferable Secret Key] with [`SignedPublicKey`].
+//! Those key objects can encrypt and decrypt [`Message`]s, as well as produce and verify signatures
+//! within messages. They can also produce and verify [`DetachedSignature`]s.
 //!
 //! [Transferable Public Key]: https://www.rfc-editor.org/rfc/rfc9580#name-transferable-public-keys
 //! [Transferable Secret Key]: https://www.rfc-editor.org/rfc/rfc9580#name-transferable-secret-keys
@@ -15,60 +16,78 @@
 //!
 //! Messages can be produced with [`MessageBuilder`], existing messages are handled with [`Message`].
 //!
+//! # Example
+//!
+//! The following example program:
+//!
+//! - Generates a pair of [`SignedSecretKey`] ([Transferable Public Key]) and
+//!   [`SignedPublicKey`] ([Transferable Secret Key]).
+//! - Builds a [Message] that is encrypted to the first subkey of the [Transferable Public Key].
+//! - Decrypts that [Message] again, using the [Transferable Secret Key].
+//!
 //! ```rust
-//! # const DATA :&'static [u8] = b"Hello World";
-//! # use rand::thread_rng;
-//! # use pgp::composed::{self, KeyType, KeyDetails, SecretKeyParamsBuilder};
-//! # use pgp::errors::Result;
-//! # use pgp::packet::{self, KeyFlags, UserAttribute, SignatureVersionSpecific, UserId};
-//! use pgp::crypto::{self, sym::SymmetricKeyAlgorithm, hash::HashAlgorithm, public_key::PublicKeyAlgorithm};
-//! use pgp::types::{self, SigningKey, VerifyingKey, CompressionAlgorithm, KeyDetails as _, Password};
-//! use smallvec::*;
-//! #
-//! # let mut key_params = SecretKeyParamsBuilder::default();
-//! # key_params
-//! #     .key_type(KeyType::Rsa(2048))
-//! #     .can_certify(false)
-//! #     .can_sign(true)
-//! #     .primary_user_id("Me <me@example.com>".into())
-//! #     .preferred_symmetric_algorithms(smallvec![
-//! #          SymmetricKeyAlgorithm::AES256,
-//! #     ])
-//! #     .preferred_hash_algorithms(smallvec![
-//! #          HashAlgorithm::Sha256,
-//! #     ])
-//! #     .preferred_compression_algorithms(smallvec![
-//! #          CompressionAlgorithm::ZLIB,
-//! #     ]);
-//! # let secret_key_params = key_params.build().expect("Must be able to create secret key params");
-//! # let signed_secret_key = secret_key_params.generate(thread_rng()).expect("Failed to generate a plain key.");
-//! # let public_key = signed_secret_key.public_key();
-//! use pgp::{packet::{Signature, SignatureConfig, PacketTrait}, types::Timestamp};
+//! use pgp::{
+//!     composed::{
+//!         KeyType, Message, MessageBuilder, SecretKeyParamsBuilder, SignedPublicKey,
+//!         SignedSecretKey, SubkeyParamsBuilder,
+//!     },
+//!     crypto::{ecc_curve::ECCCurve, hash::HashAlgorithm, sym::SymmetricKeyAlgorithm},
+//!     errors::Result,
+//!     types::Password,
+//! };
+//! use rand::thread_rng;
+//! use smallvec::smallvec;
 //!
-//! let signing_key = &signed_secret_key.primary_key;
-//! let verification_key = public_key;
+//! let mut rng = thread_rng();
 //!
+//! // Configure the shape of an OpenPGP Transferable Secret Key that we want to generate
+//! let mut key_params = SecretKeyParamsBuilder::default();
+//! key_params
+//!     .key_type(KeyType::Ed25519Legacy)
+//!     .can_certify(false)
+//!     .can_sign(true)
+//!     .primary_user_id("Me <me@example.com>".into())
+//!     .preferred_symmetric_algorithms(smallvec![SymmetricKeyAlgorithm::AES128,])
+//!     .preferred_hash_algorithms(smallvec![HashAlgorithm::Sha256,])
+//!     .preferred_compression_algorithms(smallvec![])
+//!     .subkeys(vec![SubkeyParamsBuilder::default()
+//!         .key_type(KeyType::ECDH(ECCCurve::Curve25519))
+//!         .can_encrypt(true)
+//!         .build()
+//!         .expect("Must be able to create subkey")]);
+//! let secret_key_params = key_params
+//!     .build()
+//!     .expect("Must be able to create secret key params");
 //!
-//! let passwd_fn = Password::empty();
+//! // Generate the configured Transferable Secret Key
+//! let signed_secret_key: SignedSecretKey = secret_key_params
+//!     .generate(&mut rng)
+//!     .expect("Failed to generate a plain key.");
 //!
-//! let now = Timestamp::now();
+//! // Extract a Transferable Public Key
+//! let signed_public_key: SignedPublicKey = signed_secret_key.to_public_key();
 //!
-//! let mut sig_cfg = SignatureConfig::v4(packet::SignatureType::Binary, PublicKeyAlgorithm::RSA, HashAlgorithm::Sha256);
-//! sig_cfg.hashed_subpackets = vec![
-//!     packet::Subpacket::regular(packet::SubpacketData::SignatureCreationTime(now)).unwrap(),
-//!     packet::Subpacket::regular(packet::SubpacketData::IssuerFingerprint(signing_key.fingerprint())).unwrap(),
-//! ];
+//! // Encrypt a message to the public key
+//! const DATA: &'static [u8] = b"Hello World";
 //!
-//! let signature_packet = sig_cfg
-//!      .sign(&*signing_key, &passwd_fn, DATA)
-//!      .expect("Should sign");
+//! let mut builder = MessageBuilder::from_bytes("plaintext.txt", DATA)
+//!     .seipd_v1(&mut rng, SymmetricKeyAlgorithm::AES128);
+//! builder
+//!     .encrypt_to_key(&mut rng, &signed_public_key.public_subkeys[0])
+//!     .expect("enryption to subkey works");
 //!
-//! let mut signature_bytes = Vec::with_capacity(1024);
-//! signature_packet.to_writer_with_header(&mut signature_bytes).expect("Write must succeed");
+//! // A binary representation of the encrypted OpenPGP message
+//! let encrypted: Vec<u8> = builder.to_vec(&mut rng).unwrap();
 //!
-//! signature_packet
-//!      .verify(&*verification_key, DATA)
-//!      .expect("Failed to validate signature");
+//! // Parse the message and decrypt it with the secret key
+//! let mut message = Message::from_bytes(&encrypted[..]).expect("parse message");
+//! let mut decrypted = message
+//!     .decrypt(&Password::empty(), &signed_secret_key)
+//!     .expect("decrypt message");
+//!
+//! let plaintext = decrypted.as_data_string().expect("get data");
+//!
+//! assert_eq!(plaintext.as_bytes(), DATA);
 //! ```
 
 mod any;
--- src/packet.rs
+++ src/packet.rs
@@ -1,6 +1,84 @@
 //! # Packet module
 //!
-//! Handles everything in relationship to packets.
+//! Handles everything in relation to
+//! [OpenPGP packets](https://www.rfc-editor.org/rfc/rfc9580#name-packet-syntax).
+//!
+//! Every OpenPGP object consists of a set of "packets". Usually, those are used in
+//! [`composed`](crate::composed) objects, such as Transferable Public Keys, or Messages.
+//!
+//! Users of rPGP will usually only use this low-level packet functionality indirectly via
+//! composed objects.
+//!
+//! However, users are able to use this low-level module, to implement operations at the packet
+//! level. Be aware that the packet level API makes no attempt at being safe to use:
+//! Implementing operations on raw packets requires a deep understanding of the OpenPGP format.
+//!
+//! # Example
+//!
+//! The following example program:
+//!
+//! - Generates a pair of bare key packets (which could be used as the primary key in a composed
+//!   key object): A [`SecretKey`] and the corresponding [`PublicKey`].
+//! - Configures a binary data signature using a [`SignatureConfig`].
+//! - Produces a [`Signature`] packet with the secret key packet.
+//! - Verifies that [`Signature`] packet against the same data, using the public key packet.
+//!   This operation checks if the cryptographic signature was indeed issued by the key holder
+//!   of that public key packet.
+//!
+//! ```
+//! use pgp::{
+//!     composed::KeyType,
+//!     crypto::{hash::HashAlgorithm, public_key::PublicKeyAlgorithm},
+//!     packet::{
+//!         PubKeyInner, PublicKey, SecretKey, SignatureConfig, SignatureType, Subpacket,
+//!         SubpacketData,
+//!     },
+//!     types::{KeyDetails, KeyVersion, Password, Timestamp},
+//! };
+//! use rand::thread_rng;
+//!
+//! let mut rng = thread_rng();
+//!
+//! let now = Timestamp::now();
+//!
+//! // Generate a pair of bare key packets (a "SecretKey" and a "PublicKey").
+//! //
+//! // (In a composed "SignedSecretKey" or "SignedPublicKey" object,
+//! // such packets would serve as primary keys.)
+//! let (public_params, secret_params) = KeyType::Ed25519Legacy
+//!     .generate(&mut rng)
+//!     .expect("generate key");
+//! let pub_key_inner = PubKeyInner::new(
+//!     KeyVersion::V4,
+//!     KeyType::Ed25519Legacy.to_alg(),
+//!     now,
+//!     None,
+//!     public_params,
+//! )
+//! .expect("create inner public key");
+//! let pub_key = PublicKey::from_inner(pub_key_inner).expect("create public key");
+//! let sec_key = SecretKey::new(pub_key.clone(), secret_params).expect("create secret key");
+//!
+//! let mut sig_cfg = SignatureConfig::v4(
+//!     SignatureType::Binary,
+//!     PublicKeyAlgorithm::RSA,
+//!     HashAlgorithm::Sha256,
+//! );
+//! sig_cfg.hashed_subpackets = vec![
+//!     Subpacket::regular(SubpacketData::SignatureCreationTime(now)).unwrap(),
+//!     Subpacket::regular(SubpacketData::IssuerFingerprint(pub_key.fingerprint())).unwrap(),
+//! ];
+//!
+//! const DATA: &'static [u8] = b"Hello World";
+//!
+//! let signature_packet = sig_cfg
+//!     .sign(&sec_key, &Password::empty(), DATA)
+//!     .expect("create signature packet");
+//!
+//! signature_packet
+//!     .verify(&pub_key, DATA)
+//!     .expect("Failed to validate signature");
+//! ```
 
 mod header;
 mod many;
