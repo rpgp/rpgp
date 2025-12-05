ed6a93cbaf5f7b36844417bfc7b62980d8699d36
Heiko Schaefer <heiko@schaefer.name>
2025-12-05 17:05:24 +0100

    reorganize

--- src/types/params/plain_secret.rs
+++ src/types/params/plain_secret.rs
@@ -104,7 +104,10 @@ impl PlainSecretParams {
                 let key_size = pub_params.sym_alg.key_size();
                 let key = i.take_bytes(key_size)?;
 
-                let key = aead_key::SecretKey { key: key.to_vec() };
+                let key = aead_key::SecretKey {
+                    key: key.to_vec(),
+                    sym_alg: pub_params.sym_alg,
+                };
                 Self::AEAD(key)
             }
             (
@@ -424,7 +427,7 @@ impl PlainSecretParams {
                     encrypted,
                 },
             ) => {
-                let PublicParams::AEAD(pub_param) = pub_params else {
+                let PublicParams::AEAD(_pub_param) = pub_params else {
                     bail!("inconsistent key state");
                 };
 
@@ -436,7 +439,6 @@ impl PlainSecretParams {
                         EskType::V3_4 => 3,
                         EskType::V6 => 6,
                     },
-                    sym_alg: pub_param.sym_alg,
                     salt,
                 })?
             }
