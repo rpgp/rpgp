cf1102b42616b8cc4bb100b1889e96ecc07ede36
Heiko Schaefer <heiko@schaefer.name>
2026-01-27 01:17:43 +0700

    add one more test

--- tests/persistent_symmetric.rs
+++ tests/persistent_symmetric.rs
@@ -85,6 +85,41 @@ fn psk_openpgp_js_seipdv2() {
 }
 
 #[test]
+fn psk_openpgp_js_seipdv2_mixed() {
+    // A message in which the SEIPD container uses a different symmetric algorithm than the
+    // persistent symmetric key and pkesk encryption.
+
+    let psk = get_psk();
+
+    let seipdv2 = File::open(Path::new(
+        "tests/persistent-symmetric/openpgp-js/seipdv2-mixed-algo.msg",
+    ))
+    .unwrap();
+    let (msg, _) = Message::from_armor(BufReader::new(seipdv2)).unwrap();
+
+    let mut ring = TheRing::default();
+
+    if let Message::Encrypted { esk, .. } = &msg {
+        assert_eq!(esk.len(), 1);
+
+        let Esk::PublicKeyEncryptedSessionKey(pkesk) = &esk[0] else {
+            unimplemented!()
+        };
+
+        let sk = psk
+            .decrypt(&Password::empty(), pkesk.values().unwrap(), EskType::V6)
+            .unwrap()
+            .unwrap();
+
+        ring.session_keys.push(sk);
+    }
+
+    let (msg, _res) = msg.decrypt_the_ring(ring, false).unwrap();
+
+    eprintln!("{:?}", msg);
+}
+
+#[test]
 fn psk_openpgp_js_signed() {
     let psk = get_psk();
 
